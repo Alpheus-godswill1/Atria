972260f51bccaa6eb503369ce62099447270cd90
Alpheus Godswill <Alpheus-godswill1@users.noreply.github.com>
2025-11-07 18:14:37 +0100

    feat(logging): integrate structured logging into contact form workflow
    - Added logging configuration with timestamp and log levels
    - Logged form submissions, JSON file read/write operations
    - Included error logging for exception cases
    - Improved visibility into request handling flow

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, render_template, jsonify, redirect, url_for, flash
 import os, json
+import logging
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename="Atria.log", encoding='utf-8', level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "dev_forbidden_key_to_be_used!!")
 
@@ -30,18 +33,25 @@ def contact():
 
 @app.route("/contactform/", methods=["GET", "POST"])
 def contactform():
+    logger.info("The form was submitted: ")
     if request.method == "POST":
         form_data = request.form.to_dict()
         try:
             if os.path.exists(json_path):
                 with open(json_path, 'r', encoding='utf-8') as f:
                    data =  json.load(f)
+                   logger.info("Loading to see if there is data in json file")
+             else:
+                data = []
+                logger.info("No JSON file found. Creating a new one")
 
             data.append(form_data)
             with open(json_path, 'w', encoding='utf-8') as f:
                 json.dump(data, f, indent=4)
+                logger.info("Data saved to JSON file successfully")
         except Exception as e:
             return {"Error":str(e)}, 500
+            logger.error("An error occurred: %s", e)
             exit()
         flash("Message was sent successfully!!")
         return redirect(url_for("contact"))
